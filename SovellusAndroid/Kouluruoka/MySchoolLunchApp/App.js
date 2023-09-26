d36db4b766657966c604fa83eee8ec98742b0416
Cerveku <art.karimaki@gmail.com>
2023-09-26 20:56:13 +0300

    p

--- SovellusAndroid/Kouluruoka/MySchoolLunchApp/App.js
+++ SovellusAndroid/Kouluruoka/MySchoolLunchApp/App.js
@@ -1,32 +1,46 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
-import * as firebase from 'firebase';
+import { Text, View, StyleSheet } from 'react-native';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, onValue } from 'firebase/database';
 import * as Notifications from 'expo-notifications';
 
 export default function App() {
-  const [lunch, setLunch] = useState("");
+  const [lunch, setLunch] = useState("Testilounas");
 
   useEffect(() => {
+    console.log('useEffect triggered'); // Debuggausta varten
     // Firebase-konfiguraatio ja alustus
-    const firebaseConfig = { /* ... */ };
-    if (!firebase.apps.length) {
-      firebase.initializeApp(firebaseConfig);
+    const firebaseConfig = {
+      apiKey: "AIzaSyBYBBYzCyeqCHXPa86ISj0_ode0xh9R2xU",
+      authDomain: "myschoollunchapp-d6970.firebaseapp.com",
+      databaseURL: "https://myschoollunchapp-d6970-default-rtdb.europe-west1.firebasedatabase.app",
+      projectId: "myschoollunchapp-d6970",
+      storageBucket: "myschoollunchapp-d6970.appspot.com",
+      messagingSenderId: "677352536871",
+      appId: "1:677352536871:web:62073bbb2dded209a3c702",
+      measurementId: "G-LTVRRM1JKN"
+    };
+
+    if (!getDatabase().apps.length) {
+      initializeApp(firebaseConfig);
     }
 
     // Hae päivän lounas Firebase-tietokannasta
-    const db = firebase.database().ref("weeks");
-  db.on('value', (snapshot) => {
-    const data = snapshot.val();
-    const today = new Date();
-    const currentWeek = getWeekNumber(today); // Voit tehdä oman funktion viikon numeron selvittämiseksi
-    const currentDay = getDay(today); // esim. 'ma', 'ti', 'ke'...
+    const dbRef = ref(getDatabase(), "weeks");
+    onValue(dbRef, (snapshot) => {
+      const data = snapshot.val();
+      console.log("Firebase data:", data);  // Debuggaus
+      const today = new Date();
+      const currentWeek = getWeekNumber(today); // Voit tehdä oman funktion viikon numeron selvittämiseksi
+      const currentDay = getDay(today); // esim. 'ma', 'ti', 'ke'...
+      
 
-    if (data[currentWeek] && data[currentWeek][currentDay]) {
-      setLunch(data[currentWeek][currentDay]);
-    } else {
-      setLunch("Ei tietoa");
-    }
-  });
+      if (data[currentWeek] && data[currentWeek][currentDay]) {
+        setLunch(data[currentWeek][currentDay]);
+      } else {
+        setLunch("Ei tietoa");
+      }
+    });
 
     // Aseta push-ilmoitus
     const trigger = {
@@ -39,8 +53,23 @@ export default function App() {
   }, [lunch]);
 
   return (
-    <View>
-      <Text>Päivän lounas: {lunch}</Text>
+    <View style={styles.container}>
+      <Text style={styles.headerText}>My School Lunch App</Text>
+      <Text style={styles.lunchText}>Päivän lounas: {lunch}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  lunchText: {
+    fontSize: 18,
+  },
+});
